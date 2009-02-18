3ab1fea61d5d0c2abba164030a29d0106a21965f
john.tabet <john.tabet@77adfa16-fc89-11dd-9d1d-173cc1d06e3c>
2009-02-18 10:54:50 +0000

    Now with noun frequencies that are actually sorted by frequency!

--- basalisk-java/src/autoslog_helper/NounListGenerator.java
+++ basalisk-java/src/autoslog_helper/NounListGenerator.java
@@ -2,8 +2,17 @@ package autoslog_helper;
 
 import java.io.FileWriter;
 import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collection;
 import java.util.Collections;
+import java.util.Comparator;
 import java.util.Hashtable;
+import java.util.Iterator;
+import java.util.List;
+import java.util.Map;
+import java.util.SortedSet;
+import java.util.TreeMap;
+import java.util.Vector;
 
 import word_tools.WordTools;
 
@@ -15,28 +24,31 @@ public class NounListGenerator {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		Hashtable<String, Integer> mucCount = WordTools.countWordsInDocs("muc3-listfile");
+
+		List<Map.Entry<String, Integer>> sortByValue = new ArrayList<Map.Entry<String, Integer>>(mucCount.entrySet());
+		Collections.sort(sortByValue, new Comparator<Map.Entry>(){
+			public int compare(Map.Entry e1, Map.Entry e2){
+				Integer i1 = (Integer) e1.getValue();
+				Integer i2 = (Integer) e2.getValue();
+				return i2.compareTo(i1);
+			}
+		});
 		
-		ArrayList<Integer> counts = new ArrayList<Integer>();
-		for(Integer i: mucCount.values()){
-			counts.add(i);
-		}
-		Collections.sort(counts);
-		
-		ArrayList<String> words = new ArrayList<String>();
-		for(String s: mucCount.keySet()){
-			words.add(s);
-		}
-		Collections.sort(words);
+	
+//        System.out.println("letter - freq");
+//        System.out.println("-------------");
+//        for(Map.Entry e : sortByValue) {
+//            System.out.printf("%-15s%d%n", e.getKey(), e.getValue());
+//        }
 		
 		//Create a file writer to print word count
 		FileWriter out = null;
 		try{
 			out = new FileWriter("mucFrequentWords.txt");
 		
-			//Print the word count to the file
-			for(int i = words.size() - 1; i >= 0; i--){
-				out.write(words.get(i) + "\n");
-			}
+	        for(Map.Entry e : sortByValue) {
+	          	out.write(e.getKey().toString()  + "\n");
+	        }
 			out.close();
 		}
 		catch (Exception e){
