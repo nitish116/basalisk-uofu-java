194c0bddd0eb91460fe375af8005e3aef9891f99
john.tabet <john.tabet@77adfa16-fc89-11dd-9d1d-173cc1d06e3c>
2009-02-18 07:11:33 +0000

    Can now count the words in given set of input files

---
+++ basalisk-java/src/word_tools/WordTools.java
@@ -0,0 +1,130 @@
+package word_tools;
+
+import java.util.Enumeration;
+import java.util.Hashtable;
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.File;
+import java.util.Scanner;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
+
+public class WordTools {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Hashtable<String, Integer> mucCount = countWordsInDocs("muc3-listfile");
+		
+		ArrayList<Integer> counts = new ArrayList<Integer>();
+		for(Integer i: mucCount.values()){
+			counts.add(i);
+		}
+		Collections.sort(counts);
+		
+		ArrayList<String> words = new ArrayList<String>();
+		for(String s: mucCount.keySet()){
+			words.add(s);
+		}
+		Collections.sort(words);
+		
+		System.out.println("First sorted word: " + words.get(words.size() - 1));
+		System.out.println("Word Count: " + counts.get(counts.size() - 1));
+		
+	}
+	
+	public static Hashtable<String, Integer> countWords(String fileName){
+		Hashtable<String, Integer> wordCount = new Hashtable<String, Integer>();
+		
+		File inFile = new File(fileName);
+		
+		if(!inFile.exists()){
+			System.out.println("Error counting words in document: File \"" + inFile.getAbsolutePath() + "\" does not exist.");
+			return null;
+		}
+		
+		System.out.println("Counting the words in file \"" + inFile.getAbsolutePath() + "\".");
+		
+		Scanner in = null;
+		
+		try{
+			in = new Scanner(inFile);
+		}
+		catch(Exception e){
+			System.out.println(e.getMessage());
+		}
+
+		
+		while(in.hasNext()){
+			String word = in.next().trim();
+			word = stripPunctuation(word);
+			if(wordCount.get(word) != null){
+				int oldCount = wordCount.get(word);
+				wordCount.put(word.toLowerCase(), oldCount + 1);
+			}
+			else{
+				wordCount.put(word.toLowerCase(), 1);
+			}
+		}
+		
+		in.close();
+		
+		return wordCount;
+	}
+	
+	public static String stripPunctuation(String word){
+		//Basic punctuation
+		word = word.replaceAll("[\\[\\]\\(\\)\".,;/!~?]+", "");
+		//'s removal
+		word = word.replaceAll("('s)|('S)", "");
+		//' at the end removal
+		word = word.replaceAll("'$", "");
+		return word;
+	}
+	
+	public static Hashtable<String, Integer> countWordsInDocs(String listFile){
+		Hashtable<String, Integer> wordCount = new Hashtable<String, Integer>();
+		
+		File inFile = new File(listFile);
+		
+		if(!inFile.exists()){
+			System.out.println("Error reading listfile to count words: File \"" + inFile.getAbsolutePath() + "\" does not exist.");
+			return null;
+		}
+		
+		Scanner in = null;
+		
+		try{
+			in = new Scanner(inFile);
+		}
+		catch(Exception e){
+			System.out.println(e.getMessage());
+		}
+		
+		while(in.hasNext()){
+			String nextFile = in.nextLine().trim();
+			Hashtable<String, Integer> temp = countWords(nextFile);
+			Enumeration<String> e = temp.keys();
+			
+			while(e.hasMoreElements()){
+				String word = e.nextElement();
+				int count = temp.get(word);
+				
+				if(wordCount.get(word) == null){
+					wordCount.put(word, 1);
+				}
+				else{
+					int oldCount = wordCount.get(word);
+					wordCount.put(word, count + oldCount);
+				}
+			}
+		}
+		
+		in.close();
+		return wordCount;
+	}
+
+}
